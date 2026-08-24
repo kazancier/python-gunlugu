9f4ff02bacd26a028762cdb40e00df4fcdef5865
ilk commit hey <kazancier@gmail.com>
2026-08-24 11:01:38 +0300

    Klasörler düzenlendi, Readme ye ilk not yazıldı

---
+++ src/python_gunlugu/week1/parity.py
@@ -0,0 +1,33 @@
+"""
+
+x = int(input("What's x?"))
+
+if x % 2 == 0:
+    print("x is even")
+else:
+    print("x is odd")
+
+    """
+
+def main():
+    x = int(input("What's x? " ))
+    if is_even(x):
+        print("x is even")
+    else:   
+        print("x is odd")
+
+"""
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+"""
+"""
+def is_even(n):
+    return True if n % 2 == 0 else False
+"""
+
+def is_even(n):
+    return n % 2 == 0
+main()
