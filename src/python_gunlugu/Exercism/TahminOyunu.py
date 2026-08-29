876f42162162d8c025778ffa946ef89a412b2112
ilk commit hey <kazancier@gmail.com>
2026-08-29 17:50:36 +0300

    tahmin oyunu düzenleme

--- src/python_gunlugu/Exercism/TahminOyunu.py
+++ src/python_gunlugu/Exercism/TahminOyunu.py
@@ -4,8 +4,8 @@ i = True
 rand_num = random.randint(0,100)
 while i == True:
     num = int(input("Guess the number?: "))
-    if not (isinstance(num,int) and num > 0):
-        print("Lütfen pozitif bir tamsayı girin.")
+    if num <= 0:
+        print("Lütfen pozitif bir sayı girin.")
     elif rand_num == int(num):
         print(f"Bildin sayı {num}.")
         i = False
